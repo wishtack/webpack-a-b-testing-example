fdb8a30750980d80d7fcf8d7791bffb1e5804b19
Younes JAAIDI <yjaaidi@gmail.com>
2017-02-24 20:45:14 +0100

    Webpack setup.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('app.css'),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
